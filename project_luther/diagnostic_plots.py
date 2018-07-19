e697631cf9e3f02985e7776efb361e8dde6fcb3a
Auste Mastaviciute <auste.m@gmail.com>
2018-07-19 13:44:46 -0700

    Commented out annotations from the diagnostic_plots

--- project_luther/diagnostic_plots.py
+++ project_luther/diagnostic_plots.py
@@ -64,12 +64,12 @@ def diagnostic_plots(X, y, model_fit=None):
     plot_lm_1.axes[0].set_ylabel('Residuals');
 
     # annotations
-    abs_resid = model_abs_resid.sort_values(ascending=False)
-    abs_resid_top_3 = abs_resid[:3]
-    for i in abs_resid_top_3.index:
-        plot_lm_1.axes[0].annotate(i,
-                                   xy=(model_fitted_y[i],
-                                       model_residuals[i]));
+#     abs_resid = model_abs_resid.sort_values(ascending=False)
+#     abs_resid_top_3 = abs_resid[:3]
+#     for i in abs_resid_top_3.index:
+#         plot_lm_1.axes[0].annotate(i,
+#                                    xy=(model_fitted_y[i],
+#                                        model_residuals[i]));
 
     # Normal Q-Q Plot
     QQ = ProbPlot(model_norm_residuals)
@@ -77,13 +77,14 @@ def diagnostic_plots(X, y, model_fit=None):
     plot_lm_2.axes[0].set_title('Normal Q-Q')
     plot_lm_2.axes[0].set_xlabel('Theoretical Quantiles')
     plot_lm_2.axes[0].set_ylabel('Standardized Residuals');
+    
     # annotations
-    abs_norm_resid = np.flip(np.argsort(np.abs(model_norm_residuals)), 0)
-    abs_norm_resid_top_3 = abs_norm_resid[:3]
-    for r, i in enumerate(abs_norm_resid_top_3):
-        plot_lm_2.axes[0].annotate(i,
-                                   xy=(np.flip(QQ.theoretical_quantiles, 0)[r],
-                                       model_norm_residuals[i]));
+#     abs_norm_resid = np.flip(np.argsort(np.abs(model_norm_residuals)), 0)
+#     abs_norm_resid_top_3 = abs_norm_resid[:3]
+#     for r, i in enumerate(abs_norm_resid_top_3):
+#         plot_lm_2.axes[0].annotate(i,
+#                                    xy=(np.flip(QQ.theoretical_quantiles, 0)[r],
+#                                        model_norm_residuals[i]));
 
     # Scale vs Location Plot
     plot_lm_3 = plt.figure()
@@ -97,11 +98,11 @@ def diagnostic_plots(X, y, model_fit=None):
     plot_lm_3.axes[0].set_xlabel('Fitted values')
     plot_lm_3.axes[0].set_ylabel('$\sqrt{|Standardized Residuals|}$');
 
-    #annotations
-    abs_sq_norm_resid = np.flip(np.argsort(model_norm_residuals_abs_sqrt), 0)
-    abs_sq_norm_resid_top_3 = abs_sq_norm_resid[:3]
-    for i in abs_norm_resid_top_3:
-        plot_lm_3.axes[0].annotate(i,
-                                  xy=(model_fitted_y[i],
-                                      model_norm_residuals_abs_sqrt[i]));
+#     #annotations
+#     abs_sq_norm_resid = np.flip(np.argsort(model_norm_residuals_abs_sqrt), 0)
+#     abs_sq_norm_resid_top_3 = abs_sq_norm_resid[:3]
+#     for i in abs_norm_resid_top_3:
+#         plot_lm_3.axes[0].annotate(i,
+#                                   xy=(model_fitted_y[i],
+#                                       model_norm_residuals_abs_sqrt[i]));
 
